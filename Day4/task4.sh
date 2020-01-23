ecfff6fabcbb0da3275dd6c307c946121b139a25
flyboy14 <student@EPBYMINW7240.minsk.epam.com>
2020-01-23 14:47:21 +0300

    improve readability

--- Day4/task4.sh
+++ Day4/task4.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 DIR=$PWD
+
 # Installing openjdk
+
 sudo yum -y install java wget
 
 # Installing oracle
+
 cd /opt/
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f001e99d882ca7/jdk-13.0.2_linux-x64_bin.tar.gz"
 sudo tar xzf jdk-13.0.2_linux-x64_bin.tar.gz
 cd jdk-13.0.2
 
 # Setting alternatives
+
 sudo alternatives --install /usr/bin/java java /opt/jdk-13.0.2/bin/java 2
 sudo alternatives --config java <<< 1
 
 # Installing htop
+
 sudo yum install htop
 
 # Installing docker-ce v 18.06.1
+
 sudo yum -y install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install docker-ce-18.06.1.ce-3.el7
