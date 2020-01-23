1a8a3f35f537aa1823526eb28f02db43629077b7
flyboy14 <student@EPBYMINW7240.minsk.epam.com>
2020-01-23 18:57:30 +0300

    cleanup

--- Day4/task4.sh
+++ Day4/task4.sh
@@ -8,17 +8,14 @@ sudo yum -y install java wget
 
 # Installing oracle
 
-cd /opt
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f001e99d882ca7/jdk-13.0.2_linux-x64_bin.tar.gz"
-sudo tar xzf jdk-13.0.2_linux-x64_bin.tar.gz
-cd jdk-13.0.2
+sudo tar xzf jdk-13.0.2_linux-x64_bin.tar.gz -C /opt
 
 # Setting alternatives
 
 sudo alternatives --install /usr/bin/java java /opt/jdk-13.0.2/bin/java 2
 sudo alternatives --config java <<< 1
 
-cd -
 # Installing htop
 
 sudo yum -y install epel-release
@@ -42,9 +39,9 @@ echo "5,30 12-15 * * 1 rm /tmp/*.zip" | crontab -
 # Install mongodb
 
 sudo wget "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.2.2.tgz"
-sudo tar -xvzf mongodb-linux-x86_64-rhel70-4.2.2.tgz 
+sudo tar -xzf mongodb-linux-x86_64-rhel70-4.2.2.tgz -C /opt
 
-sudo cp mongodb-linux-x86_64-rhel70-4.2.2/bin/* /usr/local/bin/
+sudo cp /opt/mongodb-linux-x86_64-rhel70-4.2.2/bin/* /usr/local/bin/
 sudo mkdir -p /var/lib/mongodb /var/lib/mongo /data/db/log /var/log/mongodb
 sudo groupadd mongod
 sudo useradd mongod -g mongod
@@ -53,9 +50,12 @@ sudo chown -Rv mongod:mongod /var/log/mongodb
 sudo chown -Rv mongod:mongod /var/lib/mongo 
 sudo chown -Rv mongod:mongod /var/lib/mongodb
 
-cd -
 sudo cp mongod.service /etc/systemd/system/
 
 # Start mongodb service
 
 sudo systemctl start mongod
+
+# Cleanup
+
+sudo rm *gz*
