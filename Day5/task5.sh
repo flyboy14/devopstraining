04dbd2c82b4d6c48ff0fd0a6ed3df0a99a88a726
flyboy14 <student@EPBYMINW7240.minsk.epam.com>
2020-01-24 15:32:24 +0300

    change user passwd w/o prompting

--- Day5/task5.sh
+++ Day5/task5.sh
@@ -6,6 +6,7 @@ sudo useradd -g wheel super_user
 
 sudo cat /etc/sudoers > sudoers.save
 sudo bash -c "cat sudoers.save | grep NOPASS | sed \"s/# %wheel/%wheel/\" >> /etc/sudoers"
+echo "Added super_user user"
 
 for i in {1..20}
 do
@@ -13,12 +14,13 @@ do
 	sudo -u super_user sudo useradd -p -U -u "$(( 9000+10*$i+$i+$i/10 ))" -g "$(( 7000+10*$i+$i+$i/10 ))" -s /bin/sh -m user$i -c "Sample user with password and homedir"
 
 	sudo usermod -aG user$i user$i
-	sudo passwd user$i
+	echo 2222 | sudo passwd --stdin "user$i" > /dev/null
 done
+echo "Created 20 users and changed their passwords to 2222"
 
 sudo groupadd even
 sudo groupadd odd
-
+echo "Added groups even and odd"
 
 for i in {1..20}
 do
@@ -30,22 +32,29 @@ do
 
 	fi
 done
+echo "Added needed users to odd and even groups"
 
 echo "" > /tmp/user.info
 for i in {1..20}
 do
 	echo "$(cat /etc/passwd | grep user$i | head -n 1 | awk -F ":" "{print \$1,\$6 }") $(last | grep super_ | sed "s/  */@/g" |awk -F "@" '{print $6}')" >> /tmp/user.info
 done 
-
+echo "Copied 20 user's info to /tmp/user.info"
 
 sudo bash -c "echo -e \"#!/bin/bash\n\nwhoami\" > /home/user1/whoami.sh"
 sudo chmod +x /home/user1/whoami.sh
+echo "Created /home/user1/shoami.sh"
 
+# Grant permissions to sudo
 sudo usermod -aG wheel user2
-sudo -u user2 sudo cp /home/user1/whoami.sh /home/user3/whoami.sh
 
+sudo -u user2 sudo cp /home/user1/whoami.sh /home/user3/
+echo "Copied /home/user1/whoami.sh to /home/user3/"
+
+# Grant permissions to user3 home folder
 sudo setfacl -R -m user:user2:rx ~user3
 
+echo "executing /home/user3 as user2:"
 sudo -u user2 bash /home/user3/whoami.sh 
 
 sudo setenforce Permissive
