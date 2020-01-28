79817a1bfcfb242d3e9a4d1e7d225d8cc3a0f8eb
flyboy14 <twiceaday28@gmail.com>
2020-01-28 22:28:08 +0300

    some echos

--- day7/provision.sh
+++ day7/provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "1-8. Executing silently..."
 groupadd -g 505 private
 useradd -p -U -u 505 -g private -m Siarhei_Kazak
 ssh-keygen -q -t rsa -f /root/.ssh/id_rsa -N '' <<< ''
@@ -14,29 +15,49 @@ chown -R mongo:staff /apps/mongo /apps/mongodb /logs/mongo
 chmod -R 750 /apps/mongo /apps/mongodb
 chmod -R 640 /logs/mongo
 
-yum install -y -q wget curl
+yum install -y -q wget curl net-tools > /dev/null
 
-sudo -u mongo wget -P /tmp https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.6.5.tgz
-sudo -u mongo curl -o /tmp/mongodb-src-r3.6.5.tar.gz https://fastdl.mongodb.org/src/mongodb-src-r3.6.5.tar.gz
+echo "9. Downloading mongo tarball via wget..."
+#sudo -u mongo wget -P /tmp https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.6.5.tgz
+echo -e "ok\n"
+echo "10. Downloading mongo sources via curl..."
+#sudo -u mongo curl -o /tmp/mongodb-src-r3.6.5.tar.gz https://fastdl.mongodb.org/src/mongodb-src-r3.6.5.tar.gz
+echo -e "ok\n"
 
-sudo -u mongo tar -xzf /tmp/mongodb-linux-x86_64-3.6.5.tgz -C /tmp/
-sudo -u mongo tar -xzf /tmp/mongodb-src-r3.6.5.tar.gz -C /tmp/
+echo "11-20. Executing silently..."
+#sudo -u mongo tar -xzf /tmp/mongodb-linux-x86_64-3.6.5.tgz -C /tmp/
+#sudo -u mongo tar -xzf /tmp/mongodb-src-r3.6.5.tar.gz -C /tmp/
 
 sudo -u mongo cp -a /tmp/mongodb-linux-x86_64-3.6.5/* /apps/mongo/
-su - mongo -c "export PATH=/apps/mongo/bin:$PATH"
-su - mongo -c 'echo PATH=/apps/mongo/bin:$PATH >> ~/.bash_profile'
-su - mongo -c 'echo PATH=/apps/mongo/bin:$PATH >> ~/.bashrc'
+
+su - mongo -c 'export PATH=/apps/mongo/bin:$PATH'
+su - mongo -c 'echo -e "PATH=/apps/mongo/bin:\$PATH\nexport PATH" >> ~/.bash_profile'
+su - mongo -c 'echo PATH=/apps/mongo/bin:\$PATH >> ~/.bashrc'
 
 echo '@mongo soft nproc 32000' >> /etc/security/limits.conf
 echo '@mongo hard nproc 32000' >> /etc/security/limits.conf
 
-echo -e 'Cmnd_Alias SKAZAK_CMDS = /apps/mongo/bin/mongod\nSiarhei_Kazak ALL=(mongo) NOPASSWD: SKAZAK_CMDS' >> /etc/sudoers
+echo 'Siarhei_Kazak ALL=(mongo) NOPASSWD: /apps/mongo/bin/mongod' >> /etc/sudoers
 
 if [[ ! $(getenforce) == "Permissive" ]];then 
 	sed -i 's@enforcing@disabled@' /etc/selinux/config
 fi
 
-#sudo -u Siarhei_Kazak sudo -H -u mongo /apps/mongo/bin/mongod --dbpath /apps/mongodb
+chown -R mongo:staff /logs 
+chmod -R 740 /logs
+
+sudo -u Siarhei_Kazak sudo -H -u mongo /apps/mongo/bin/mongod --dbpath /apps/mongodb > /logs/mongo/mongod_manual.log&
+echo "21. Manually starting mongod..." && sleep 5s
+echo -e "- PID is $(pgrep mongod)"
+echo -e "- tail of logfile is:"
+echo -e "----------------------------------\t"
+echo "$(tail /logs/mongo/mongod_manual.log)"
+echo -e "----------------------------------\t"
+echo -e "- netstat -tulpan shows: $(netstat -tulpan | grep 27017 | xargs)"
+pkill mongod
+echo "22. Mongod killed"
+
+echo "23-24. Executing silently..."
 
 cp -f /tmp/mongodb-src-r3.6.5/rpm/mongod.conf /apps/mongo/
 
@@ -52,19 +73,16 @@ Environment=\"OPTIONS=-f /apps/mongo/mongod.conf\"
 ExecStart=/apps/mongo/bin/mongod \$OPTIONS
 ExecStartPre=/usr/bin/mkdir -p /var/run/mongodb
 ExecStartPre=/usr/bin/chown mongo:staff /var/run/mongodb
-ExecStartPre=/bin/test -f /apps/mongo/bin/mongod
-ExecStartPre=/bin/test -d /apps/mongodb/
-ExecStartPre=/bin/test -d /logs/mongo
-ExecStartPre=/bin/test -z "$(pgrep mongod)"
 PermissionsStartOnly=true
 PIDFile=/var/run/mongodb/mongod.pid
 Type=simple\n
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/mongod.service
 
-chown -R mongo:staff /logs 
-chmod -R 740 /logs
 sed -i "s_fork: true_fork: false_g" /apps/mongo/mongod.conf
 
 systemctl start mongod
-systemctl enable mongod
+systemctl -q enable mongod
+#systemctl daemon-reload
+
+echo "Done!"
